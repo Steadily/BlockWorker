b68a4b2085bc7f82619a04a653175e49faa5f0c6
Steadily <747258227@qq.com>
2025-07-04 23:07:47 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,30 @@
+package main
 
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	BuildVersion   = ""
+	BuildGoVersion = ""
+	BuildTime      = ""
+)
+
+func init() {
+	if BuildTime == "" {
+		BuildTime = time.Now().String()
+	}
+	if BuildVersion == "" {
+		BuildVersion = "just_build_for_develop"
+	}
+	fmt.Printf("BuildTime: %s\nBuildGoVersion: %s\nBuildGoVersion: %s\n", BuildTime, BuildGoVersion, BuildVersion)
+}
